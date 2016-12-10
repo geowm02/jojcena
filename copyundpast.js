b0bfee2538c547af877dd7ef7c1bbb6f870c30ee
geowm02 <GeoWM2002@gmail.com>
2016-12-10 11:51:31 -0500

    Update copyundpast.js

--- copyundpast.js
+++ copyundpast.js
@@ -3,7 +3,7 @@ var h1tags = document.getElementsByTagName("h1");
 h1tags[0].onclick = mahnegus;	
 }
 function mahnegus(){
-	this.innerHTML = "Click harder";
+
 	var randomcolor = '#'+Math.floor(Math.random()*16777215).toString(16);
 this.style.color = randomcolor;	
 }
